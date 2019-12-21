27e13a1fce2d3a9cd87bc49e2c8fff96df3a8cbd
Alexey <89641234561@mail.ru>
2019-12-21 14:22:32 +0800

    Вот на такой сейчас стадии. Есть затыки, о них напишу в телеграм

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,60 @@
 "use strict";
 
 var gulp = require("gulp");
+var imagemin = require("gulp-imagemin");
+var webp = require("gulp-webp");
+var rename = require("gulp-rename");
+var svgstore = require("gulp-svgstore");
 var plumber = require("gulp-plumber");
 var sourcemap = require("gulp-sourcemaps");
 var less = require("gulp-less");
 var postcss = require("gulp-postcss");
+var csso = require("gulp-csso");
 var autoprefixer = require("autoprefixer");
 var server = require("browser-sync").create();
+var posthtml = require("gulp-posthtml");
+var include = require("posthtml-include");
+var jsmin = require("gulp-uglify");
+
+gulp.task("js", function() {
+  return gulp.src("source/js/index.js")
+    .pipe(jsmin())
+    .pipe(rename("index.min.js"))
+    .pipe(gulp.dest("source/js"));
+ });
+
+gulp.task("html", function() {
+  return gulp.src("source/*.html")
+   .pipe(posthtml([
+     include()
+   ]))
+   .pipe(gulp.dest("source")); 
+});
+
+gulp.task("images", function() {
+  return gulp.src("source/img/**/*.{png,jpg,svg}")
+    .pipe(imagemin([
+      imagemin.optipng({optimizationLevel: 3}),
+      imagemin.jpegtran({progressive: true}),
+      imagemin.svgo()
+    ]))
+    .pipe(gulp.dest("source/img"));
+});
+
+gulp.task("sprite", function () {
+  return gulp.src("source/img/icons/*.svg")
+    .pipe(svgstore({
+      inlineSvg: true
+    }))
+    .pipe(rename("symbols.svg"))
+    .pipe(gulp.dest("source/img"));
+});  
+
+gulp.task("webp", function () {
+  return gulp.src("source/img/content/*.jpg")
+    .pipe(webp({quality: 90})) 
+    .pipe(gulp.dest("source/img/content"));
+}); 
 
 gulp.task("css", function () {
   return gulp.src("source/less/style.less")
@@ -16,6 +64,8 @@ gulp.task("css", function () {
     .pipe(postcss([
       autoprefixer()
     ]))
+    .pipe(csso())
+    .pipe(rename("style.min.css"))
     .pipe(sourcemap.write("."))
     .pipe(gulp.dest("source/css"))
     .pipe(server.stream());
